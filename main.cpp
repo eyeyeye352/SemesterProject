3f7cade7fb328e459f2be131cbd55dd1aeeb6dbb
eyeyeye352 <eyeyeye352@qq.com>
2025-03-27 23:47:09 +0800

    第一次提交版本。

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+
+    //啊八八八八八八测试
+}
